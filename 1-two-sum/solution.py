b8fd34ddc188585f6cae1ec38e8a8836f6ac3a5b
BELLUMPOOJITHA1234 <bellumpoojitha@gmail.com>
2026-09-05 14:39:40 +0530

    Two Sum - 2026-09-05T09:09:38.784Z

---
+++ 1-two-sum/solution.py
@@ -0,0 +1,7 @@
+// 1738 ms | 19.7 MB
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
