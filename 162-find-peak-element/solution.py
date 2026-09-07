c2f3e1cf14acf4c5f68f7cbb2edaf2ea323cdbd5
BELLUMPOOJITHA1234 <bellumpoojitha@gmail.com>
2026-09-07 07:18:09 +0530

    Find Peak Element - 2026-09-07T01:48:08.748Z

---
+++ 162-find-peak-element/solution.py
@@ -0,0 +1,14 @@
+// 0 ms | 19.1 MB
+class Solution:
+    def findPeakElement(self, nums):
+        l = 0
+        h = len(nums) - 1
+        while l < h:
+            mid = (l + h) // 2
+
+            if nums[mid] > nums[mid + 1]:
+                h = mid
+            else:
+                l = mid + 1
+
+        return l
