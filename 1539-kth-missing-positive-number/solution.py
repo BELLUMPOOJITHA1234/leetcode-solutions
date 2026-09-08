0b080298cb990ef62266d16dc1b9f5c4198fe248
BELLUMPOOJITHA1234 <bellumpoojitha@gmail.com>
2026-09-08 19:54:24 +0530

    Kth Missing Positive Number - 2026-09-08T14:24:23.234Z

---
+++ 1539-kth-missing-positive-number/solution.py
@@ -0,0 +1,13 @@
+// 69 ms | 19.3 MB
+class Solution:
+    def findKthPositive(self, arr, k):
+        num = 1
+
+        while k > 0:
+            if num not in arr:
+                k -= 1
+
+            if k == 0:
+                return num
+
+            num += 1
