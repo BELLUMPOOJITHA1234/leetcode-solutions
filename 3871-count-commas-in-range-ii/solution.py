a08b8322492b53c12b3fac87c19881fafc43335a
BELLUMPOOJITHA1234 <bellumpoojitha@gmail.com>
2026-09-09 20:15:59 +0530

    Count Commas in Range II - 2026-09-09T14:45:58.873Z

---
+++ 3871-count-commas-in-range-ii/solution.py
@@ -0,0 +1,16 @@
+// 0 ms | 19.3 MB
+class Solution:
+    def countCommas(self, n: int) -> int:
+        ans = 0
+        start = 1000
+        commas = 1
+
+        while start <= n:
+            end = min(n, start * 1000 - 1)
+
+            ans += (end - start + 1) * commas
+
+            start *= 1000
+            commas += 1
+
+        return ans
