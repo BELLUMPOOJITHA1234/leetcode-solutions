a7346885dfaf27e1c26ee251ad357b8d5732ad7e
BELLUMPOOJITHA1234 <bellumpoojitha@gmail.com>
2026-09-11 16:54:49 +0530

    Intersection of Two Arrays II - 2026-09-11T11:24:49.140Z

---
+++ 350-intersection-of-two-arrays-ii/solution.py
@@ -0,0 +1,15 @@
+// 3 ms | 19.2 MB
+class Solution:
+    def intersect(self, nums1, nums2):
+        count = {}
+        ans = []
+
+        for x in nums1:
+            count[x] = count.get(x, 0) + 1
+
+        for x in nums2:
+            if count.get(x, 0) > 0:
+                ans.append(x)
+                count[x] -= 1
+
+        return ans
