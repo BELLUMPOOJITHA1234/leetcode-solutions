9066e38be795512926907ec3ff33d7bc862de711
BELLUMPOOJITHA1234 <bellumpoojitha@gmail.com>
2026-09-24 11:03:41 +0530

    Maximum Subarray - 2026-09-24T05:33:41.119Z

---
+++ 53-maximum-subarray/solution.py
@@ -0,0 +1,9 @@
+// 35 ms | 31.3 MB
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        cs=nums[0]
+        ms=nums[0]
+        for i in range(1,len(nums)):
+            cs=max(nums[i],cs+nums[i])
+            ms=max(cs,ms)
+        return ms
