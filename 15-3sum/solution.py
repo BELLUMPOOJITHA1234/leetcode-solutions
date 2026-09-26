b49ff79a162073b899528feac65896ccade1ee56
BELLUMPOOJITHA1234 <bellumpoojitha@gmail.com>
2026-09-26 18:23:43 +0530

    3Sum - 2026-09-26T12:53:42.576Z

---
+++ 15-3sum/solution.py
@@ -0,0 +1,33 @@
+// 644 ms | 22.2 MB
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums.sort()
+        ans = []
+
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            l = i + 1
+            r = len(nums) - 1
+
+            while l < r:
+                total = nums[i] + nums[l] + nums[r]
+
+                if total == 0:
+                    ans.append([nums[i], nums[l], nums[r]])
+
+                    l += 1
+                    r -= 1
+
+                    # skip duplicates
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+
+                elif total < 0:
+                    l += 1
+                else:
+                    r -= 1
+
+        return ans
+       
